9518903ca161baa4d0b7107cf36b1e3269aa5ec8
henricolas23 <henricolas23filizola@gmail.com>
2025-05-28 15:03:30 -0300

    add

--- idade.c
+++ idade.c
@@ -2,11 +2,14 @@
 
 int main (int argc, char* argv []){
     
-    
-    
-    
-    
-    
+    int idade = 0;
+    int ano = 0;
+
+    printf("digite a idade e o ano: %d. \n");
+    scanf("%d %d", &idade, &ano);
+
+    printf("idade informada: %d. \n", idade);
+    printf("ano informado: %d. \n", ano);
     
     
     return 0;
