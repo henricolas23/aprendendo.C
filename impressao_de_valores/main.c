fbc1d178831c4c4390a7e049da0a5f96c26ecddd
henricolas23 <henricolas23filizola@gmail.com>
2026-08-30 14:05:16 -0300

    add commit date 30/08/26 at 14:04h

---
+++ impressao_de_valores/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*run this pragrma using the console pauser or add your own getch, system */
+
+int main(int argc, char *argv[]){
+
+    printf("Oi, tudo bem? Eu tenho 20 anos e estou programando em C.\n");
+
+    printf("valor inteiro: %d \n", 10);
+    printf("Valor real: %f \n", 3.14159265358979323846);
+    printf("Valor real  com apenas duas casa : %.2f \n", 3.14159265358979323846);
+    printf("Dado de texto: %c \n", 'a');
+    printf("Dado de texto: %s \n", "Ola, tudo bem?");
+
+
+    return 0;
+}
